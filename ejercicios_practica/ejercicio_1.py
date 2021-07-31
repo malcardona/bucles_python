c7ca2fbaa1d55867a8f6a09fad8e1331e1a6ac39
Alejandro L. Cardona <acardona@unc.edu.ar>
2021-07-30 22:34:41 -0300

    Update ejercicio_1.py

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -14,12 +14,14 @@ x = 0
 # para que el "while" itere siempre que <x sea menor a 6>
 # Además, complete la línea de código necesaria para que
 # el valor de "x" incremente "1" en cada iteración
-condicion = False
+condicion = True
 
 # reemplace "condicion" por lo que crea necesario
-while condicion:    
+while x < 6:    
     print("Valor de x =", x)
     # Coloque la línea de código para que "X" incremente "1"
+    x += 1
+
 
 x = 5
 # Dado el siguiente "while", complete la condicion
@@ -27,8 +29,9 @@ x = 5
 # Además, complete la línea de código necesaria para que
 # el valor de "x" decremente "1" en cada iteración
 
-while condicion:    # reemplace "condicion" por lo que crea necesario
+while x > 0:    # reemplace "condicion" por lo que crea necesario
     print("Valor de x =", x)
     # Coloque la línea de código para que "X" decremente "1"
+    x -= 1
 
 print("terminamos!")
